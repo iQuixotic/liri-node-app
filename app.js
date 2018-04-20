5a25a0fad93d913c64d361c83fca49f8e000eb88
Trey <treyw1850@gmail.com>
2018-04-20 11:25:53 -0700

    changed file structure

---
+++ app.js
@@ -0,0 +1,40 @@
+
+// twitter npm
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+// spotify installation
+npm install --save node-spotify-api
+
+// spotify's search function
+search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
+
+// spotify npm
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: <your spotify client id>,
+  secret: <your spotify client secret>
+});
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
+
