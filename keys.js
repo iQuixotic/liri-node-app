8a7baa48867db04b163448944fbcee67e99ed337
Trey <treyw1850@gmail.com>
2018-04-21 12:54:30 -0700

    songs, movies, and tweets are searchable

---
+++ keys.js
@@ -0,0 +1,17 @@
+
+
+// console.log('this is loaded');
+
+
+
+exports.twitter = {
+  consumer_key: 'z2HEE37cH6GfdFXZHMQgljXvK',
+  consumer_secret: 'pQyLL5E37Af3zLRSann0DaLlZZaZvbWeWeHSZN4OncLEzIEAvD',
+  access_token_key: '987310469005414401-tgoCfuLC6faC0bcyLcLebzf67LK1wxo',
+  access_token_secret: 'hMFpIljmKzIcxwHG7AfFVxaxdx8Sgw7Vxfy2IJHPvvW78'
+};
+
+exports.spotify = {
+  id: '38f47bf60bb84e36aa63baffee5d33ea',
+  secret: '04fbb5c1603e49b098116b77db8627f1'
+};
